bd9dbe475d4a948f7179edcb5cf633ab0ed4ad36
Guo Du <mrduguo@gmail.com>
2016-11-12 23:04:04 +0000

    enable -x docker to skip docker support

--- src/main/groovy/com/github/mrduguo/gradle/buildscript/docker/DockerPlugin.groovy
+++ src/main/groovy/com/github/mrduguo/gradle/buildscript/docker/DockerPlugin.groovy
@@ -1,8 +1,8 @@
 package com.github.mrduguo.gradle.buildscript.docker
 
-import com.github.mrduguo.gradle.buildscript.dist.DistPlugin
 import com.github.mrduguo.gradle.buildscript.utils.Env
 import com.github.mrduguo.gradle.buildscript.utils.ProjectHelper
+import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.BasePlugin
@@ -16,13 +16,16 @@ class DockerPlugin implements Plugin<Project> {
     void apply(Project project) {
         IS_DOCKER_PROJECT = project.file('src/main/docker/Dockerfile').exists()
         if (IS_DOCKER_PROJECT) {
-            if (project.getTasksByName('assemble', true).empty) {
-                project.getPlugins().apply(BasePlugin)
-            }
+            project.getTasks().create('docker', DefaultTask.class)
+            if (!project.gradle.startParameter.excludedTaskNames.contains('docker')) {
+                if (project.getTasksByName('assemble', true).empty) {
+                    project.getPlugins().apply(BasePlugin)
+                }
 
-            def dockerBuildTask = setupBuildTask(project)
-            setupResourceTask(project,dockerBuildTask)
-            setupPushTask(project, dockerBuildTask)
+                def dockerBuildTask = setupBuildTask(project)
+                setupResourceTask(project, dockerBuildTask)
+                setupPushTask(project, dockerBuildTask)
+            }
         }
     }
 
@@ -33,7 +36,7 @@ class DockerPlugin implements Plugin<Project> {
         dockerBuildTask
     }
 
-    void setupResourceTask(Project project,def dockerBuildTask) {
+    void setupResourceTask(Project project, def dockerBuildTask) {
         def processResourceTask = project.getTasks().create('dockerResources', ProcessResources.class)
         processResourceTask.from(project.file('src/main/docker'))
         processResourceTask.expand(project.properties)
